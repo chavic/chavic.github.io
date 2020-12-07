6cadde5d80e7fa08a8b5d6a2c2ac64f0ba4b2a63
chavic <chabundavictor@gmail.com>
2020-12-07 20:34:47 +0100

    fix(dev): fixed vscode errors with railwind

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,8 +1,18 @@
 module.exports = {
   extends: [
+    "stylelint-config-recommended",
     'stylelint-config-standard'
   ],
   // add your custom config here
   // https://stylelint.io/user-guide/configuration
-  rules: {}
+  rules: {
+    "at-rule-no-unknown": [true, {
+      "ignoreAtRules": [
+        "extends",
+        "tailwind"
+      ]
+    }],
+    "block-no-empty": null,
+    "unit-whitelist": ["em", "rem", "s"]
+  }
 }
