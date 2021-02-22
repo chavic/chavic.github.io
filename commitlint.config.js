756fc376255827ad8e8b77f50d82ba74ea4a3e34
chavic <chabundavictor@gmail.com>
2021-02-22 10:03:31 +0200

    docs: updated some readme files

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,3 +1,9 @@
+// Check out the link below to learn the conventional commit style
+// https://github.com/conventional-changelog/commitlint
+// General format follows type(scope?): title
+// Type must be one of the following [build, chore, ci, docs, feat, fix, perf, refactor, revert, style, test]
+// you cant also have multiple scopes by sepertatibing them with , or /
+
 module.exports = {
   extends: ['@commitlint/config-conventional'],
 }
