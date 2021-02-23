d2c05548f8f3cea657a059e903f5eeb0da392d24
chavic <chabundavictor@gmail.com>
2021-02-23 07:53:17 +0200

    style: added theme and images

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        'dark-primary': '181818',
+        'dark-accent': '747474',
+        'text-color': '4A4A4A',
+      },
+    },
+  },
+}
